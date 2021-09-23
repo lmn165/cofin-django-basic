4b7a8c16cd9744924b7a6c0b848c0023c456b52e
lmn165 <lastalking@naver.com>
2021-09-23 18:18:36 +0900

    first commit

---
+++ admin/sorting/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+import unittest
+# Create your tests here.
+from sorting.models import MySum, Palindrome
+
+class TestMySum(unittest.TestCase):
+
+    def test_one_to_ten_sum_1(self):
+        instance = MySum()
+        instance.start_number = 1
+        instance.end_number = 11
+        res = instance.one_to_ten_sum_2()
+        print(f'My Expected Value is {res}')
+        self.assertEqual(res, 55)
+
+    def test_palindrome(self):
+        instance = Palindrome()
+        # instance.str_to_list("A man, a plan, a canal: Panama")
+        # print(str_arr)
+        self.assertEqual(instance.isPalindrome(instance.str_to_list("A man, a plan, a canal: Panama"))["Result"], True)
+
+if __name__ == '__main__':
+    unittest.main()
